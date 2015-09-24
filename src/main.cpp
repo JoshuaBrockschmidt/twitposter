67efe949c86754000ddf47c680a3572531c41a62
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2015-09-24 00:44:39 -0700

    Parse command-line for a tweet to send

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,23 @@ std::string consumerSecret    = "TODO";
 std::string accessTokenKey    = "TODO";
 std::string accessTokenSecret = "TODO";
 
-int main() {
-	TwitPoster twitPosterObj(consumerKey, consumerSecret,
-			      accessTokenKey, accessTokenSecret);
-	twitPosterObj.postTweet("This tweet was sent via TwitPoster");
-	try {
-		twitPosterObj.postTweet("");
-	} catch (TwitPoster::bad_tweet& e) {
-		std::cout << "This error should be thrown: "
-			  << e.what()
+int main(int argc, char **argv) {
+	if (argc == 1) {
+	        std::cerr << argv[0]
+			  << ": too few arguments"
 			  << std::endl;
+		exit(EXIT_FAILURE);
 	}
 	try {
-		twitPosterObj.postTweet("This status update exceeds the 140 character limit. Thus an instance of TwitPoster::bad_tweet should be thrown when this string is passed as a parameter of TwitPoster::postTweet. If a section of this tweet is posted to Twitter, then something has gone wrong");
-	} catch (TwitPoster::bad_tweet& e) {
-		std::cout << "This error should be thrown: "
+		TwitPoster twitPosterObj(consumerKey, consumerSecret,
+					 accessTokenKey, accessTokenSecret);
+		twitPosterObj.postTweet(std::string(argv[1]));
+		//DEBUG
+		std::cout << twitPosterObj.getLastWebResponse() << std::endl;
+		//EOF DEBUG
+	} catch (std::runtime_error& e) {
+		std::cerr << argv[0]
+			  << ": "
 			  << e.what()
 			  << std::endl;
 	}
